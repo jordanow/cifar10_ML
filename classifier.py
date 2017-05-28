52bb93c56a06fd9b60d747d99b78171bf24a052e
Shailendra Pal <shailendra.pal@leezair.com>
2017-05-28 16:54:59 +1000

    Completed LR and Random forest

--- classifier.py
+++ classifier.py
@@ -1,11 +1,28 @@
 from sklearn.ensemble import RandomForestClassifier as rfc
 from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import IncrementalPCA as IPCA
 
 
 def rfclassifier(X, y, estimators=50):
-    print('Classification using estimators', estimators)
+    print('Random Forest Classification using estimators', estimators)
     classifier = rfc(n_estimators=estimators)
     X_train = preprocessing.scale(X.astype(float))
     y_train = y
-    classifier.fit(X, y)
-    return classifier
+    classifier.fit(X_train, y_train)
+    return classifier, X_train
+
+
+def dimensional_reduction(X, y, num_features=200, batch_size=500):
+    ipca = IPCA(n_components=num_features, batch_size=batch_size)
+    ipca.fit(X, y)
+    return ipca.transform(X)
+
+
+def logistic_classifier(X, y, num_features):
+    print('Logistic Classification with features', num_features)
+    classifier = LogisticRegression()
+    X_train = dimensional_reduction(X.astype(float), y, num_features=num_features)
+    y_train = y
+    classifier.fit(X_train, y_train)
+    return classifier, X_train
--- main.py
+++ main.py
@@ -15,14 +15,35 @@ X_test, y_test = fr.get_image_data(DATA_FILES_FOLDER, 'test')
 # print(X.shape, y.shape)
 # print(X_test.shape, y_test.shape)
 
-rfc = classifier.rfclassifier(X, y, 100)
-y_predicted = rfc.predict(X_test)
 
-accuracy, precision, recall, f1_score = st.get_statistics(y_test, y_predicted)
-print('Accuracy is :', accuracy)
-print('Precision is :', precision)
-print('Recall is :', recall)
-print('F1 Score is :', f1_score)
+# Get the statistics for Random forest classifier
+def get_rfc_stats(estimators):
+    rfc, X_train = classifier.rfclassifier(X, y, estimators)
+    y_predicted = rfc.predict(X_train)
+
+    accuracy, precision, recall, f1_score = st.get_statistics(
+        y, y_predicted)
+    print('Accuracy is :', accuracy)
+    print('Precision is :', precision)
+    print('Recall is :', recall)
+    print('F1 Score is :', f1_score)
+
+
+# Get the statistics for logistic regression classifier
+def get_LR_stats(components):
+    cl, X_train = classifier.logistic_classifier(X, y, components)
+    y_predicted = cl.predict(X_train)
+
+    accuracy, precision, recall, f1_score = st.get_statistics(
+        y, y_predicted)
+    print('Accuracy is :', accuracy)
+    print('Precision is :', precision)
+    print('Recall is :', recall)
+    print('F1 Score is :', f1_score)
+
+
+get_LR_stats(300)
+# get_rfc_stats(1)
 
 # print("Test set score", classifier.score(X_test, y_test))
 # print("Predicted set score", classifier.score(X_test, y_predicted))
@@ -31,5 +52,6 @@ print('F1 Score is :', f1_score)
 # # conf_mat = confusion_matrix(y_test, y_predicted)
 # # print(conf_mat)
 # # plot_confusion_matrix(conf_mat, y_test)
+
 
 print('Ended computation at', dt.datetime.now())
