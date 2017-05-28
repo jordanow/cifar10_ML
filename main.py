c31a29ec72c4571825ed274be63852884a9d2fd5
Shailendra Pal <shailendra.pal@leezair.com>
2017-05-28 20:41:29 +1000

    Get train accuracy

--- main.py
+++ main.py
@@ -19,31 +19,53 @@ X_test, y_test = fr.get_image_data(DATA_FILES_FOLDER, 'test')
 # Get the statistics for Random forest classifier
 def get_rfc_stats(estimators):
     rfc, X_train = classifier.rfclassifier(X, y, estimators)
-    y_predicted = rfc.predict(X_train)
 
+    y_train_predicted = rfc.predict(X_train)
     accuracy, precision, recall, f1_score = st.get_statistics(
-        y, y_predicted)
-    print('Accuracy is :', accuracy)
-    print('Precision is :', precision)
-    print('Recall is :', recall)
-    print('F1 Score is :', f1_score)
+        y, y_train_predicted)
+    print('Accuracy for training set is :', accuracy)
+    print('Precision for training set is :', precision)
+    print('Recall for training set is :', recall)
+    print('F1 Score for training set is :', f1_score)
+
+    y_test_predicted = rfc.predict(X_test)
+    accuracy, precision, recall, f1_score = st.get_statistics(
+        y_test, y_test_predicted)
+    print('-------------------------------------------')
+    print('Accuracy for test set is :', accuracy)
+    print('Precision for test set is :', precision)
+    print('Recall for test set is :', recall)
+    print('F1 Score for test set is :', f1_score)
+    print('===========================================')
 
 
 # Get the statistics for logistic regression classifier
 def get_LR_stats(components):
     cl, X_train = classifier.logistic_classifier(X, y, components)
-    y_predicted = cl.predict(X_train)
 
+    y_train_predicted = cl.predict(X_train)
     accuracy, precision, recall, f1_score = st.get_statistics(
-        y, y_predicted)
-    print('Accuracy is :', accuracy)
-    print('Precision is :', precision)
-    print('Recall is :', recall)
-    print('F1 Score is :', f1_score)
+        y, y_train_predicted)
+    print('Accuracy for training set is :', accuracy)
+    print('Precision for training set is :', precision)
+    print('Recall for training set is :', recall)
+    print('F1 Score for training set is :', f1_score)
+
+    X_test_dimensionally_reduced = classifier.dimensional_reduction(
+        X_test, y_test, components)
+    y_test_predicted = cl.predict(X_test_dimensionally_reduced)
+    accuracy, precision, recall, f1_score = st.get_statistics(
+        y_test, y_test_predicted)
+    print('-------------------------------------------')
+    print('Accuracy for test set is :', accuracy)
+    print('Precision for test set is :', precision)
+    print('Recall for test set is :', recall)
+    print('F1 Score for test set is :', f1_score)
+    print('===========================================')
 
 
-get_LR_stats(300)
-# get_rfc_stats(1)
+# get_LR_stats(300)
+# get_rfc_stats(500)
 
 # print("Test set score", classifier.score(X_test, y_test))
 # print("Predicted set score", classifier.score(X_test, y_predicted))
